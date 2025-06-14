4fadfa85dd810cb9e6bd8582194006d545369dd5
ohanaferrari <ohanaferrari@gmail.com>
2025-06-14 16:45:17 -0300

    added real life weather data

--- src/script.js
+++ src/script.js
@@ -2,8 +2,17 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector(".temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector(".city");
+  let condition = document.querySelector(".condition");
+  let humidity = document.querySelector(".humidity");
+  let wind = document.querySelector(".wind");
+  let icon = document.querySelector(".icon");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
+  condition.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" alt="${response.data.condition.description}" />`;
 }
 
 function search(city) {
@@ -21,7 +30,41 @@ function searchSubmit(event) {
   search(city);
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let time = document.querySelector(".time");
+  time.innerHTML = ` ${hours}:${minutes}`;	
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let formattedDay = days[day];
+  let weekDay = document.querySelector(".week-day");
+  weekDay.innerHTML = `${formattedDay}`;
+}
+
 let searchForm = document.querySelector(".search-form");
 searchForm.addEventListener("submit", searchSubmit);
+
+let currentDate = new Date();
+formatDate(currentDate);
 
 search("Florianópolis");
