77e70e03bec64f4b79b7d9a501b461f3ad124e10
ohanaferrari <ohanaferrari@gmail.com>
2025-06-14 14:53:58 -0300

    created script.js and added function search

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector('.search-form-input');
+  let city = document.querySelector('.city');
+  city.innerHTML = searchInput.value;
+}
+
+let searchForm = document.querySelector('.search-form');
+searchForm.addEventListener("submit", search);
